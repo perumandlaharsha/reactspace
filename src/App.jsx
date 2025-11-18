faf310e125c5016a1a183f735b8b653f6173ee3f
perumandlaharsha <perumandlaharsha05@gmail.com>
2025-11-18 15:46:05 +0530

    React Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactSetupGuide from './components/concepts/introduction/ReactsetupGuide'
+import './App.css'
+
+export default function 
+() {
+  return (
+    <div>
+       <ReactSetupGuide />
+    </div>
+  )
+}
